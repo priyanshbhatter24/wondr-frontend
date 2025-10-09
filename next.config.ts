e206453ddf3203b4829f02a826724fea9be54053
Priyansh Bhatter <pbhatter@usc.edu>
2025-10-09 02:10:37 -0700

    Add wildcard S3 domains to image config
    - Support presigned URLs with query parameters
    - Add *.s3.amazonaws.com and *.s3.*.amazonaws.com patterns
    - Required for displaying images via presigned URLs

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "wondr-org-media.s3.us-west-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.*.amazonaws.com",
+      },
     ],
   },
 };
