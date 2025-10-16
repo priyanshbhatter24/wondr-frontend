a2a13abc97b06bfc05198e206dcc72d1d3ea8d9d
Priyansh Bhatter <pbhatter@usc.edu>
2025-10-16 02:22:35 -0700

    Add image modal component for full-size preview

---
+++ components/ImageModal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
+interface ImageModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  imageUrl: string;
+  fileName: string;
+}
+
+export function ImageModal({ isOpen, onClose, imageUrl, fileName }: ImageModalProps) {
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/80 backdrop-blur-sm z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 z-50 max-h-[90vh] max-w-[90vw] -translate-x-1/2 -translate-y-1/2 focus:outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
+          <div className="relative">
+            {/* Close button */}
+            <Dialog.Close asChild>
+              <button
+                className="absolute -top-12 right-0 rounded-full p-2 bg-white/10 hover:bg-white/20 text-white transition-colors"
+                aria-label="Close"
+              >
+                <Cross2Icon className="w-5 h-5" />
+              </button>
+            </Dialog.Close>
+
+            {/* Image */}
+            <img
+              src={imageUrl}
+              alt={fileName}
+              className="max-h-[90vh] max-w-[90vw] rounded-lg shadow-2xl object-contain"
+            />
+
+            {/* File name caption */}
+            <div className="absolute -bottom-12 left-0 right-0 text-center">
+              <p className="text-sm text-white/60">{fileName}</p>
+            </div>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
